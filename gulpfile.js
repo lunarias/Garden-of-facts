7a3dfc814cd92a135a437982fc1e0e2250311f6f
Shaina <shainamae98@gmail.com>
2021-09-26 15:25:44 -0600

    import sass styling and watch changed to sass files with gulp serve

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,23 @@
 //
 // exports.default = defaultTask
 const gulp = require('gulp');
+var sass = require('gulp-sass')(require('node-sass'));
+var concat = require('gulp-concat');
+
 const browserSync = require('browser-sync').create();
 const reload = browserSync.reload;
 
+
+// compile sass files
+gulp.task('sass', function () {
+    return gulp.src('app/style/*.scss')
+        .pipe(concat('combined.scss'))
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('app/style/cssDist/'));
+});
+
 // Static server
-gulp.task('serve', function() {
+gulp.task('serve', function () {
     browserSync.init({
         // https: {key: './localhost.key', cert: './localhost.crt'},
         server: {
@@ -18,13 +30,21 @@ gulp.task('serve', function() {
     });
 
     gulp.watch([
-      'app/*.js',
-      'app/*.html',
-      'app/*/*.html',
-      'app/images/**/*',
-      'app/scripts/**/*.js',
-      'app/style/*.css',
-      'app/style/*.scss',
-      'app/pages/*.html'
-    ]).on('change',reload);
+        'app/*.js',
+        'app/*.html',
+        'app/*/*.html',
+        'app/images/**/*',
+        'app/scripts/**/*.js',
+        'app/style/*.css',
+        'app/pages/*.html'
+    ]).on('change', reload);
+
+    // watch sass files
+    gulp.watch(['app/style/*.scss']).on('change', function () {
+        var gulpRes = gulp.src('app/style/*.scss')
+            .pipe(concat('combined.scss'))
+            .pipe(sass().on('error', sass.logError))
+            .pipe(gulp.dest('app/style/cssDist/'));
+        reload();
+    });
 });
