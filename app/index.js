a4bab5800fd6701e1199a5975f04c9fde800e1c6
unknown <jcai8203@gmail.com>
2021-09-27 22:06:45 -0600

    added animations for seeds

---
+++ app/index.js
@@ -0,0 +1,24 @@
+
+
+$(document).ready(function () {
+  $(".seed").hide();
+
+  $('#seed-spot-1').click(function(){
+    $("#seed-1").fadeIn();
+  })
+
+  $('#seed-spot-2').click(function(){
+    $("#seed-2").fadeIn();
+  })
+
+  $('#seed-spot-3').click(function(){
+    $("#seed-3").fadeIn();
+  })
+
+  $('#seed-spot-4').click(function(){
+    $("#seed-4").fadeIn();
+  })
+ 
+ 
+
+})
